fa956b99aba5e2c3cf595d38c2f2c14919586fa1
Ctlinker <166874035+Ctlinker@users.noreply.github.com>
2025-12-16 21:34:43 +0000

    feat(expriment-v2): implemented `resolveStackFrame` fn better alt to src's `adaptiveLookUp`
    note: currently untested

--- src-alt/lib/lookup-functions.ts
+++ src-alt/lib/lookup-functions.ts
@@ -1,7 +1,10 @@
+import { FrameConfidenceScore } from "../types/confidence-evaluator";
 import { FrameCollection } from "../types/frame-collector";
 import { StackSearchCriteria } from "../types/lookup-functions";
 import { StackFrame } from "../types/stackframe-descriptor";
 import { Nullable } from "../types/type-utils";
+import { FrameConfidenceEvaluator } from "./confidence-evaluator";
+import { detectRuntime } from "./detect-runtime";
 import { FrameCollector } from "./frame-collector";
 
 /**
@@ -77,12 +80,110 @@ export function findStackFrame<T extends Error | FrameCollection>(
     }
 
     const match =
-      typeof symbol === "string" ? method.includes(symbol) : symbol.test(method);
+      typeof symbol === "string"
+        ? method.includes(symbol)
+        : symbol.test(method);
 
     if (!match) continue;
 
     const targetIndex = offset == null ? i : i + offset;
     return stack[targetIndex] ?? frame;
+  }
+
+  return null;
+}
+
+export interface StackFrameResolution<T> {
+  error: T;
+  options?: {
+    firefoxCompatibility?: boolean;
+    evaluateAllPossibilities?: boolean;
+    evaluateAllPosWith?: (
+      param: Array<[StackFrame, FrameConfidenceScore]>
+    ) => StackFrame | null;
+  };
+  strategies: Array<
+    {
+      runtime?: Set<"node" | "bun" | "deno" | "browser">;
+      accept?: (frame: StackFrame, collection: FrameCollection) => boolean;
+    } & (
+      | { symbol?: string | RegExp; offset: number }
+      | { symbol: string | RegExp; offset?: number }
+    )
+  >;
+}
+
+export function resolveStackFrame<T extends Error | FrameCollection>(
+  plan: StackFrameResolution<T>
+): StackFrame | null {
+  const { error, options, strategies } = plan;
+
+  if (!strategies || strategies.length === 0) {
+    console.warn(
+      "[monitext/nstack]: no strategies provided to: `resolveStackFrame`"
+    );
+    return null;
+  }
+
+  if (!error) {
+    return null;
+  }
+
+  const buf = [];
+  const collection: FrameCollection =
+    error instanceof Error ? new FrameCollector(error) : error;
+
+  for (const strategy of strategies) {
+    const { runtime, symbol, offset, accept } = strategy;
+
+    if (Array.isArray(runtime) && !runtime.includes(detectRuntime())) {
+      continue;
+    }
+
+    const frame = findStackFrame({
+      error: collection,
+      symbol: symbol as NonNullable<typeof symbol>,
+      offset: offset as NonNullable<typeof offset>,
+      options,
+    });
+
+    if (frame == null) {
+      continue;
+    }
+
+    if (typeof accept === "function" && accept(frame, collection) !== true) {
+      continue;
+    }
+
+    if (options?.evaluateAllPossibilities === true) {
+      buf.push(frame);
+    } else {
+      return frame;
+    }
+  }
+
+  if (buf.length === 1) {
+    return buf[0];
+  } else if (buf.length > 1) {
+    const evalutedFrames: [StackFrame, FrameConfidenceScore][] = buf.map(
+      (frame) => [frame, new FrameConfidenceEvaluator(frame)]
+    );
+    if (typeof options?.evaluateAllPosWith === "function") {
+      return options.evaluateAllPosWith(evalutedFrames);
+    }
+    
+    const best = evalutedFrames.reduce((prev, cur) => {
+      const { score: prevScore } = prev[1]
+      const { score } = cur[1];
+      
+      if(prevScore == null || score > prevScore){
+        return cur
+      }
+
+      return prev
+    })
+
+    return best[0];
   }
 
   return null;
